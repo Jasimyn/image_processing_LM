bc75b54ae1b4c8fe6ed27aa3e54516b83af9da50
Deng yawen <97659765+Jasimyn@users.noreply.github.com>
2024-01-29 02:40:38 +0800

    Update download.py

--- download.py
+++ download.py
@@ -1,3 +1,11 @@
+import os
+
+# 设置环境变量
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
+# 下载sentence-transformer模型
+os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir /home/xlab-app-center/model/sentence-transformer')
+
 import torch
 from modelscope import snapshot_download, AutoModel, AutoTokenizer
 import os
