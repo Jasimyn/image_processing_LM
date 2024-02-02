6f7541028ce05b3febdb48f04ac9914fa2c71528
Deng yawen <97659765+Jasimyn@users.noreply.github.com>
2024-02-02 22:14:24 +0800

    Update download.py

--- download.py
+++ download.py
@@ -9,4 +9,4 @@ os.system('huggingface-cli download --resume-download sentence-transformers/para
 import torch
 from modelscope import snapshot_download, AutoModel, AutoTokenizer
 import os
-model_dir = snapshot_download('Shanghai_AI_Laboratory/internlm2-chat-7b', cache_dir='/home/xlab-app-center/model')
+model_dir = snapshot_download('Shanghai_AI_Laboratory/internlm-chat-7b', cache_dir='/home/xlab-app-center/model')
